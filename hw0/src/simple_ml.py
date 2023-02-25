79c7efdbdca67f70639b5edf47a5db95824b335c
ChenyangXu <1127862434@qq.com>
2023-02-25 23:45:47 +0800

    hw0: finish 2-layer nn epoch test

--- hw0/src/simple_ml.py
+++ hw0/src/simple_ml.py
@@ -49,44 +49,44 @@ def parse_mnist(image_filename, label_filename):
     """
     ### BEGIN YOUR CODE
     with gzip.open(image_filename, 'rb') as f:
-        
+
         # MNIST stores data in big-endian format
         # load header variables
         _ = struct.unpack('>I', f.read(4))[0]
         num_images = struct.unpack('>I', f.read(4))[0]
         num_rows = struct.unpack('>I', f.read(4))[0]
         num_cols = struct.unpack('>I', f.read(4))[0]
-        
+
         # load images pixels
         images_data = f.read(num_images * num_rows * num_cols)
         images = struct.unpack('>' + 'B' * len(images_data), images_data)
-        
+
         # reshape images
         images = np.array(images).reshape(num_images, num_rows * num_cols)
-        
+
         # normalize the dataset
         images = images / 255
-        
+
         # convert the data to fp32
         images = images.astype(np.float32)
-        
+
     with gzip.open(label_filename, 'rb') as f:
-        
+
         # load header variables
         _ = struct.unpack('>I', f.read(4))[0]
         num_labels = struct.unpack('>I', f.read(4))[0]
-        
+
         # load images pixels
         labels_data = f.read(num_labels)
         labels = struct.unpack('>' + 'B' * len(labels_data), labels_data)
-        
+
         # reshape images
         labels = np.array(labels)
-        
+
         # convert the data to uint8
         labels = labels.astype(np.uint8)
-        
-    return (images, labels)   
+
+    return (images, labels)
     ### END YOUR CODE
 
 
@@ -129,7 +129,7 @@ def softmax_regression_epoch(X, y, theta, lr = 0.1, batch=100):
         None
     """
     ### BEGIN YOUR CODE
-    
+
     # m: the num of testcase
     # n: the num of features
     # k: the num of labels
@@ -137,28 +137,28 @@ def softmax_regression_epoch(X, y, theta, lr = 0.1, batch=100):
     # y:     mx1
     # theta: nxk
     # gradient as to theta: nxk
-    
+
     # divide testset into different epoches
     iterations = (y.shape[0] + batch - 1) // batch
     for i in range(iterations):
-        
+
         # theta := theta - lr / batch * sum(gradient)
         # gradient = X^T * (Z - Iy)
         batched_X = X[i*batch: (i+1)*batch]
         batched_y = y[i*batch: (i+1)*batch]
-        
+
         # Z = normalize(exp(X * theta)), mxk dim
         Z = np.exp(batched_X @ theta)
         Z = Z / np.sum(Z, axis=1, keepdims=True)
-        
+
         # Iy, Iy[i][y_i] = 1, others are 0, mxk dim
         Iy = np.zeros(Z.shape)
         Iy[np.arange(Z.shape[0]), batched_y] = 1
-        
+
         # the sum of gradient, nxk dim
         grad = batched_X.T @ (Z - Iy)
         theta -= grad * (lr / batch)
-    
+
     ### END YOUR CODE
 
 
@@ -185,7 +185,111 @@ def nn_epoch(X, y, W1, W2, lr = 0.1, batch=100):
         None
     """
     ### BEGIN YOUR CODE
-    pass
+    
+    def relu(x):
+        return np.where(x > 0, x, 0)
+    
+    def d_relu(x):
+        return np.where(x > 0, 1, 0)
+    
+    def identification(x):
+        return x
+    
+    def d_identification(x):
+        return np.ones(x.shape)
+    
+    def calc_gradient_bf(X, y, W1, W2):
+        """ Derive the gradient w.r.t W1 and W2 by handwritten respectively.
+        Calculate them directly. Not a general method.
+        """
+        
+        # S = normalize(exp(sigma(X @ W1) @ W2))
+        S = np.exp(relu(X @ W1) @ W2)
+        S = S / np.sum(S, axis=1, keepdims=True)
+        
+        Iy = np.zeros(S.shape)
+        Iy[np.arange(S.shape[0]), y] = 1
+        
+        gradient_w1 = np.transpose(xx) @ ((S - Iy) @ np.transpose(W2) * d_relu(X @ W1))
+        gradient_w2 = np.transpose(relu(xx @ W1)) @ (S - Iy)
+        
+        return [
+            gradient_w1,
+            gradient_w2,
+        ]
+        
+    def calc_gradient(L, X, y, W):
+        
+        """ Do the backpropagation iteratively. The time complexity is O(L).
+        Will modify W in place.
+        
+        Args:
+            L (int): the num of layers
+            X (np.ndarray[np.float32]):
+                2D tensor, the sampled dataset, batch x input_dim
+            y (np.ndarray[np.uint8]):
+                1D tensor, the sampled labelset, batch x 1
+            W (list[np.ndarray[np.float32]]):
+                the list of parameters for each layer
+        """
+        
+        # define sigma functions and their derivatives
+        sigma = [
+            relu,
+            identification,
+        ]
+        d_sigma = [
+            d_relu,
+            d_identification,
+        ]
+        
+        # Z[i]: output of layer i
+        # Z[i + 1] = sigma[i](Z[i] @ W[i])
+        # i = 0, 1, ..., L - 1
+        # Z[1] = x
+        Z = [X] + [None] * (L)
+        for i in range(0, L):
+            Z[i + 1] = sigma[i](Z[i] @ W[i])
+            
+        # S = normalize(exp(sigma(X @ W1) @ W2))
+        S = np.exp(Z[L])
+        S = S / np.sum(S, axis=1, keepdims=True)
+        
+        Iy = np.zeros(S.shape)
+        Iy[np.arange(S.shape[0]), y] = 1
+        
+        # G[i]: cached terms of partial derivatives
+        # G[i] = (G[i + 1] * d_sigma[i](Z[i] @ W[i])) @ transpose(Z[i])
+        # i = 0, 1, ..., L
+        # G[L + 1] = S - Iy
+        G = [None] * L + [S - Iy]
+        for i in range(L - 1, -1, -1):
+            G[i] = (G[i + 1] * d_sigma[i](Z[i] @ W[i])) @ np.transpose(W[i])
+        
+        # gradient_w_i = transpose(Z[i]) @ (G[i + 1] * d_sigma[i](Z[i] @ W[i]))
+        gradients = [None] * L
+        for i in range(L - 1, -1, -1):
+            gradients[i] = np.transpose(Z[i]) @ (G[i + 1] * d_sigma[i](Z[i] @ W[i]))
+            
+        return gradients
+    
+    iterations = (X.shape[0] + batch - 1) // batch
+    for i in range(iterations):
+        xx = X[i*batch: (i+1)*batch]
+        yy = y[i*batch: (i+1)*batch]
+        layer_num = 2
+        
+        gradients_bf = calc_gradient_bf(X=xx, y=yy, W1=W1, W2=W2)
+        gradients = calc_gradient(L=layer_num, X=xx, y=yy, W=[W1, W2])
+
+        assert len(gradients_bf)==len(gradients), 'the length of the calculated gradient should be the same'
+        for i in range(len(gradients)):
+            assert (gradients_bf[i] == gradients[i]).all(), f'calculated gradient should be the same, but fail at gradient {i}'
+
+        W = [W1, W2]
+        for i in range(layer_num - 1, -1, -1):
+            W[i] -= lr / batch * gradients[i]
+        
     ### END YOUR CODE
 
 
